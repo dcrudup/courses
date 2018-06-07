bcf4b21c8fb93ecd16d160d708ab548338597a23
dcrudup <denise@crudup.net>
2018-06-07 16:46:12 -0400

    Initial Commit

---
+++ src/main/java/org/wecancodeit/courses/CourseRepository.java
@@ -0,0 +1,5 @@
+package org.wecancodeit.courses;
+
+public class CourseRepository {
+
+}
---
+++ src/test/java/org/wecancodeit/courses/CourseRepositoryTest.java
@@ -0,0 +1,14 @@
+package org.wecancodeit.courses;
+
+import org.junit.Test;
+
+public class CourseRepositoryTest {
+	
+	@Test
+	public void shouldFindACourse{
+		
+		CourseRepository underTest = new CourseRepository(course);
+	}
+	
+
+}
